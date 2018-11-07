8cc9f885b808f4af4112d9215d0aeb71eb9527f9
Mark Outridge <maou1871@edu.kadk.dk>
2018-11-07 15:16:34 +0100

    Finished Timer

---
+++ Experimental Multiplayer Game/Assets/Scripts/myTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class myTimer : MonoBehaviour {
+
+    public float timerCountdown=60;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerCountdown -= Time.deltaTime;
+        timerText.text = timerCountdown.ToString("f0");
+	}
+}
