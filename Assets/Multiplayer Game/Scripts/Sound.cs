765822cbdd4aadcd5f8a2386ea6dd08971482eb0
MathiasJP <mathiasjesspedersen@hotmail.com>
2018-11-08 14:54:22 +0100

    added new player scripts

---
+++ Assets/Multiplayer Game/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioSource Scored;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            audioSource.Play();
+        }
+
+    }
+   
+}
