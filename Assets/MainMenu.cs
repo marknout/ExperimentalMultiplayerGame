2c9104e26dc81beb6b7931e77933d4a1acb6e437
Mark Outridge <maou1871@edu.kadk.dk>
2018-11-12 14:58:00 +0100

    added start screen UI + countdown

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame () 
+    {
+        //Scene.Manager.LoadScene(ScreenManager.GetActiveScene().buildindex +1);
+    }
+
+}
---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0, 0, 70 * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
